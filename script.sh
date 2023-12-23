c91a291fc73fbbcb81fd258d574fe5da1b157642
ajeet7iitm <21f1000907@ds.study.iitm.ac.in>
2023-12-23 13:57:45 +0000

    added script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Export the GitHub username
-export GITHUB_USERNAME="<username>"
+export GITHUB_USERNAME="ajeet7iitm"
 
 # Export the GitHub repository name
-export GITHUB_REPOSITORY="se2001-<roll_number>"
+export GITHUB_REPOSITORY="se2001-21f1000907"
